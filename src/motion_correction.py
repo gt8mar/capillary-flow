f3cd4051c39b0462ddcd0e087ba683160784fbb5
gt8mar <gt8mar@yahoo.com>
2022-11-05 13:15:49 -0700

    changed chop_top to crop.py made it work seamlessly with pipeline

--- src/motion_correction.py
+++ src/motion_correction.py
@@ -14,12 +14,12 @@ try:
 except:
     pass
 
-try:
-    if __IPYTHON__:
-        get_ipython().magic('load_ext autoreload')
-        get_ipython().magic('autoreload 2')
-except NameError:
-    pass
+# try:
+#     if __IPYTHON__:
+#         get_ipython().magic('load_ext autoreload')
+#         get_ipython().magic('autoreload 2')
+# except NameError:
+#     pass
 
 logging.basicConfig(format=
                           "%(relativeCreated)12d [%(filename)s:%(funcName)20s():%(lineno)s] [%(process)d] %(message)s",
@@ -30,3 +30,32 @@ import caiman as cm
 from caiman.motion_correction import MotionCorrect, tile_and_correct, motion_correction_piecewise
 from caiman.utils.utils import download_demo
 
+""" jupiter text here """
+
+
+fnames = 'Sue_2x_3000_40_-46.tif'
+fnames = [download_demo(fnames)]     # the file will be downloaded if it doesn't already exist
+m_orig = cm.load_movie_chain(fnames)
+downsample_ratio = .2  # motion can be perceived better when downsampling in time
+m_orig.resize(1, 1, downsample_ratio).play(q_max=99.5, fr=30, magnification=2)   # play movie (press q to exit)
+
+
+""" jupiter text here """
+
+
+max_shifts = (6, 6)  # maximum allowed rigid shift in pixels (view the movie to get a sense of motion)
+strides =  (48, 48)  # create a new patch every x pixels for pw-rigid correction
+overlaps = (24, 24)  # overlap between pathes (size of patch strides+overlaps)
+num_frames_split = 100  # length in frames of each chunk of the movie (to be processed in parallel)
+max_deviation_rigid = 3   # maximum deviation allowed for patch with respect to rigid shifts
+pw_rigid = False  # flag for performing rigid or piecewise rigid motion correction
+shifts_opencv = True  # flag for correcting motion using bicubic interpolation (otherwise FFT interpolation is used)
+border_nan = 'copy'  # replicate values along the boundary (if True, fill in with NaN)
+
+
+#%% start the cluster (if a cluster already exists terminate it)
+if 'dview' in locals():
+    cm.stop_server(dview=dview)
+c, dview, n_processes = cm.cluster.setup_cluster(
+    backend='local', n_processes=None, single_thread=False)
+    
