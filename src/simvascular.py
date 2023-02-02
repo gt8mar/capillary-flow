3bb0b1c792072dc0e05b9a4b814c74b331a06adb
gt8mar <gt8mar@yahoo.com>
2023-02-02 11:44:48 -0800

    updated script to work on simvascular

--- src/simvascular.py
+++ src/simvascular.py
@@ -8,10 +8,10 @@ simvascular paths and segmentations.
 
 import os
 import numpy as np
-import sv.pathplanning as pp
+import sv
 import time
 
-home_dir = "C:\\ejerison\\capillary-flow\\data\\processed"
+home_dir = "H:\\Marcus\\Data\\sample_000"
 def select_points(coord_path, distance_path, skip = 100):
     coords = np.loadtxt(coord_path, delimiter = ',', dtype = float).astype(int)
     distances = np.loadtxt(distance_path, delimiter = ',', dtype = float)
@@ -30,24 +30,24 @@ def select_points(coord_path, distance_path, skip = 100):
     return shifted_coords, selected_dist
 
 def make_path(SET, sample, capillary):
-    coord_path = os.path.join(home_dir, str(SET), str(sample), "E_centerline", f"{SET}_{sample}_skeleton_coords_", str(capillary).zfill(2),".csv")
-    distance_path = os.path.join(home_dir, str(SET), str(sample), "D_segmented", f"{SET}_{sample}_capillary_distances_", str(capillary).zfill(2),".csv")
-    name = f"{SET}_{sample}_path_{capillary}"
-    path = pp.Path()
+    coord_path = os.path.join(home_dir, "E_centerline", "set_01_sample_000_skeleton_coords_07.csv")
+    distance_path = os.path.join(home_dir, "D_segmented", "set_01_sample_000_capillary_distances_07.csv")
+    name = "set_01_sample_000_path_07"
+    banana = sv.pathplanning.Path()
     coords, distances = select_points(coord_path, distance_path, skip = 80)
     for point in coords:
         x_coord = point[1]
         y_coord = point[0]
-        path.add_control_point([x_coord, y_coord, 0])
-    return 0
+        banana.add_control_point([float(x_coord), float(y_coord), 0.0])
+    return banana
 
 # This provided line is required at the end of a Python file
 # to call the main() function.
 if __name__ == "__main__":
     ticks = time.time()
     SET = 'set_01'
-    sample = 'sample_001'
+    sample = 'sample_000'
     capillary = '7'
-    make_path(SET, sample, capillary)
+    banana = make_path(SET, sample, capillary)
     print("--------------------")
     print("Runtime: " + str(time.time() - ticks))
