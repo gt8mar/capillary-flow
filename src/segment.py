66808158a25df2cf20fbba17d231102bc40f1b65
gt8mar <gt8mar@yahoo.com>
2023-03-22 18:15:21 -0700

    import COCO json

--- src/segment.py
+++ src/segment.py
@@ -9,6 +9,11 @@ with help from: https://huggingface.co/blog/fine-tune-segformer
 
 import torch
 import detectron2
+import os
+from detectron2.data.datasets import register_coco_instances
 
 # get data in the correct format
 # train shit lol
+
+path_to_dataset = "C:\\Users\\gt8mar\\capillary-flow\\data\\processed\\set_02\\230322_export.json"
+register_coco_instances("set_02", {}, path_to_dataset, "path/to/image/dir")
