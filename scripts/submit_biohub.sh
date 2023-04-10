a3614880d3693f46373a06a2a70d1edf4e4b025c
gt8mar <gt8mar@yahoo.com>
2023-04-10 16:59:36 -0700

    updated to run on biohub hpc

---
+++ scripts/submit_biohub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=detectron2train
+#
+#SBATCH --time=00:10:00
+#SBATCH --partition=standard
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --gres=gpu:1
+
+srun hostname
+cd ~
+python segment_hpc.py
