15f00037c759d733751593cb67a762f8d1a789d9
gt8mar <gt8mar@yahoo.com>
2023-04-12 11:07:59 -0700

    separated training and segmenting processes training runs fine.

--- src/segment.py
+++ src/segment.py
@@ -21,10 +21,16 @@ from detectron2.utils.visualizer import ColorMode
 # from detectron2.data import MetadataCatalog
 
 def main():
-    json_train = "/hpc/mydata/marcus.forst/230323_train.json"
-    json_val = "/hpc/mydata/marcus.forst/230323_val.json"
-    folder_train = "/hpc/mydata/marcus.forst/train_backgrounds_export"
-    folder_val = "/hpc/mydata/marcus.forst/val_export"
+    # json_train = "/hpc/mydata/marcus.forst/230323_train.json"
+    # json_val = "/hpc/mydata/marcus.forst/230323_val.json"
+    # folder_train = "/hpc/mydata/marcus.forst/train_backgrounds_export"
+    # folder_val = "/hpc/mydata/marcus.forst/val_export"
+
+    json_train = "D:\\Marcus\\segmentations\\230323_train.json"
+    json_val = "D:\\Marcus\\segmentations\\230323_val.json"
+    folder_train = "D:\\Marcus\\train_backgrounds_export"
+    folder_val = "D:\\Marcus\\val_export"
+    weights_path = "C:\\Users\\gt8mar\\capillary-flow\\output"
     register_coco_instances("my_dataset_train", {}, json_train, folder_train)
     register_coco_instances("my_dataset_val", {}, json_val, folder_val)
 
@@ -44,43 +50,21 @@ def main():
     cfg = get_cfg()
     cfg.INPUT.MASK_FORMAT = "bitmask" # This was a necessary addition for my segmentation files to run
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-    cfg.DATASETS.TRAIN = ("my_dataset_train",)
-    cfg.DATASETS.TEST = ()
-    cfg.DATALOADER.NUM_WORKERS = 2
-    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
-    cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
-    cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-    cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-    cfg.SOLVER.STEPS = []        # do not decay learning rate
-    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
-    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-    # NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
-
-    # model = detectron2.modeling.build_model(cfg)
-    # checkpointer = detectron2.checkpoint.DetectionCheckpointer(model)
-    # checkpointer.load(cfg.MODEL.WEIGHTS)
-
-
-    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-    trainer = DefaultTrainer(cfg) 
-    trainer.resume_or_load(resume=False)
-    trainer.train()
-
-    # # Inference should use the config with parameters that are used in training
-    # # cfg now already contains everything we've set previously. We changed it a little bit for inference:
-    # cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
-    # cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set a custom testing threshold
-    # predictor = DefaultPredictor(cfg)
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+    # This is different than in train_detectron2.py:
+    cfg.MODEL.WEIGHTS = os.path.join(weights_path,"model_final.pth")  # original input: cfg.OUTPUT_DIR
+    predictor = DefaultPredictor(cfg)
     
-    # for d in dataset_val:    
-    #     im = cv2.imread(d["file_name"])
-    #     outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
-    #     v = Visualizer(im[:, :, ::-1],
-    #                 scale=0.5, 
-    #                 instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
-    #     )
-    #     out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-    #     cv2.imshow(out.get_image()[:, :, ::-1])
+    for d in dataset_val:    
+        im = cv2.imread(d["file_name"])
+        outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+        v = Visualizer(im[:, :, ::-1],
+                    scale=0.5, 
+                    instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+        )
+        out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        plt.imshow(out.get_image()[:, :, ::-1])
+        plt.show()
 
 """
 -----------------------------------------------------------------------------
