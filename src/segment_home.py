16a7914537d4d9342e7fcb902483933130dc5e90
gt8mar <gt8mar@yahoo.com>
2023-04-13 14:40:12 -0700

    edited comments

--- src/segment_home.py
+++ src/segment_home.py
@@ -4,7 +4,6 @@ Filename: segment_model.py
 This file trains a machine learning model to
 perform instance segmentation on images. 
 By: Marcus Forst
-with help from: https://huggingface.co/blog/fine-tune-segformer
 """
 
 import torch
