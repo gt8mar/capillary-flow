0072a4f23a81e65700d8a1441e48949fdc2c39d9
gt8mar <gt8mar@yahoo.com>
2023-04-17 14:49:42 -0700

    made function to separate image files into videos

---
+++ src/tools/video_separator.py
@@ -0,0 +1,39 @@
+"""
+Filename: video_separator.py
+-----------------------
+By: Gabby Rincon
+"""
+
+import os
+import time
+import shutil
+
+def main():
+    directory_path = 'D:\\data_gabby\\230413_test - Copy'
+    filenames = os.listdir(directory_path)
+    filenames = sorted(filenames, key=lambda x: os.path.getctime(os.path.join(directory_path, x)))
+    print(filenames)
+    files = []
+    for i in range(len(filenames)):
+        imgnum = filenames[i].split('_')[-1]
+        files.append([filenames[i],filenames[i].split('_')[-2],imgnum[:len(imgnum)-5]]) #list where each element is a list of filename, vid number, image number TODO don't hardcode .tiff size. instead remove '.tiff'
+    
+    vidnum = 0
+    for i in range(len(files)):
+        if (int(files[i][2]) == 0):
+            vidnum += 1
+            dest_path = os.path.join(directory_path, "vid" + str(vidnum))
+            os.makedirs(dest_path)
+            source_path = os.path.join(directory_path, str(files[i][0]))
+            shutil.move(source_path, dest_path)
+            
+        else:
+            source_path = os.path.join(directory_path, str(files[i][0]))
+            dest_path = os.path.join(directory_path, "vid" + str(vidnum))
+            shutil.move(source_path, dest_path)        
+
+if __name__ == "__main__":
+    ticks = time.time()
+    main()
+    print("--------------------")
+    print("Runtime: " + str(time.time() - ticks))
