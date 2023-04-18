a96b8f0e9fadc15893f29c08a6ce614ca33c1f03
gt8mar <gt8mar@yahoo.com>
2023-04-18 16:20:29 -0700

    added zfill to add leading zeros to folder name

--- src/tools/video_separator.py
+++ src/tools/video_separator.py
@@ -11,7 +11,7 @@ import shutil
 def main():
     directory_path = 'D:\\data_gabby\\230413_test - Copy'
     filenames = os.listdir(directory_path)
-    filenames = sorted(filenames, key=lambda x: os.path.getctime(os.path.join(directory_path, x)))
+    filenames = sorted(filenames, key=lambda x: os.path.getctime(os.path.join(directory_path, x))) # TODO: i'm not sure we should use creation time
     print(filenames)
     files = []
     for i in range(len(filenames)):
@@ -22,14 +22,14 @@ def main():
     for i in range(len(files)):
         if (int(files[i][2]) == 0):
             vidnum += 1
-            dest_path = os.path.join(directory_path, "vid" + str(vidnum))
+            dest_path = os.path.join(directory_path, "vid" + str(vidnum).zfill(2))
             os.makedirs(dest_path)
             source_path = os.path.join(directory_path, str(files[i][0]))
             shutil.move(source_path, dest_path)
             
         else:
             source_path = os.path.join(directory_path, str(files[i][0]))
-            dest_path = os.path.join(directory_path, "vid" + str(vidnum))
+            dest_path = os.path.join(directory_path, "vid" + str(vidnum).zfill(2))
             shutil.move(source_path, dest_path)        
 
 if __name__ == "__main__":
