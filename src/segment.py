a800568cda6f1a0585ee7b702d057bf4133a85fe
gt8mar <gt8mar@yahoo.com>
2023-05-04 20:58:14 -0700

    got segment.py to work but saves in wrong folder

--- src/segment.py
+++ src/segment.py
@@ -52,9 +52,18 @@ def main():
     cfg = get_cfg()
     cfg.INPUT.MASK_FORMAT = "bitmask" # This was a necessary addition for my segmentation files to run
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.DATASETS.TRAIN = ("my_dataset_train",)
+    cfg.DATASETS.TEST = ()
+    cfg.DATALOADER.NUM_WORKERS = 2
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
     # This is different than in train_detectron2.py:
     cfg.MODEL.WEIGHTS = os.path.join(weights_path, "model_final.pth")  #os.path.join(weights_path,"model_final.pth")  # original input: cfg.OUTPUT_DIR
+    cfg.SOLVER.IMS_PER_BATCH = 2  # This is the real "batch size" commonly known to deep learning people
+    cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+    cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+    cfg.SOLVER.STEPS = []        # do not decay learning rate
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   # The "RoIHead batch size". 128 is faster, and good enough for this toy dataset (default: 512)
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
     predictor = DefaultPredictor(cfg)
     
     for d in dataset_val:    
@@ -73,7 +82,7 @@ def main():
         print(outputs["instances"].pred_classes)
         print(outputs["instances"].pred_boxes)
         out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        plt.imsave(os.path.join("/hpc/mydata/marcus.forst/segmented", sample + "_segmented.png"), 
+        plt.imsave(os.path.join(cfg.OUTPUT_DIR, str(d["file_name"]) + "_segs.png"), 
                     out.get_image()[:, :, ::-1])
 
 """
