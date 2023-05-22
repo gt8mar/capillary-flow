fc06066252203b0d70e97ebe74f1819a2e9204b7
gt8mar <gt8mar@yahoo.com>
2023-05-22 13:37:15 -0700

    no upload_data script

--- scripts/upload_data.sh
+++ scripts/upload_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source_folder="/path/to/source"   # Replace with the path to your source folder
+source_folder="/mnt/d/Marcus/data"   # Replace with the path to your source folder
 remote_folder="marcus.forst@login-01.czbiohub.org:/hpc/mydata/marcus.forst/data"   # Replace with the remote destination
 
 # Include specific subfolders
@@ -10,23 +10,42 @@ included_subfolders=(
 )
 
 # Specify the range of folder numbers
-start_number=1
-end_number=10
+start_number=9
+end_number=12
 
+# Iterate through "part" folders
+# for part_folder in "$source_folder"/part*; do
 for ((folder_number=start_number; folder_number<=end_number; folder_number++))
 do
-  source_folder="part$(printf "%02d" $folder_number)"   # Format the folder number with leading zeros if necessary
+  part_folder="part$(printf "%02d" $folder_number)"
+  echo "Processing part folder: $part_folder"
+  part_path="$source_folder/$part_folder"
 
-  rsync_command="rsync -avz"
+  if [ -d "$part_folder" ]; then
+    echo "Processing part folder: $part_folder"
+    # Iterate through "date" folders within each "part" folder
+    for date_folder in "$part_folder"/*/; do
+      echo "Processing date folder: $date_folder"
+      if [ -d "$date_folder" ]; then
+        # Iterate through "vid" folders within each "date" folder
+        for vid_folder in "$date_folder"/vid*; do
+          echo "Processing vid folder: $vid_folder"
+          if [ -d "$vid_folder" ]; then
+            rsync_command="rsync -avz"
 
-  # Add include patterns for the specified subfolders
-  for subfolder in "${included_subfolders[@]}"; do
-    rsync_command+=" --include=/$subfolder"
-  done
+            # Add include patterns for the specified subfolders
+            for subfolder in "${included_subfolders[@]}"; do
+              rsync_command+=" --include=/$subfolder"
+            done
 
-  # Exclude loose files
-  rsync_command+=" --exclude=/*"
+            # Exclude loose files
+            rsync_command+=" --exclude=/*"
 
-  # Perform the synchronization
-  eval "$rsync_command" "$source_folder" "$remote_folder"
+            # Perform the synchronization
+            eval "$rsync_command" "$vid_folder" "$remote_folder"
+          fi
+        done
+      fi
+    done
+  fi
 done
