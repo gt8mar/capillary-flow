aa2a0033d977e641c0ac8097665a50c82151e929
gt8mar <gt8mar@yahoo.com>
2023-05-25 13:48:02 -0700

    bug fixes

--- scripts/submit_test.sh
+++ scripts/submit_test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #
 #SBATCH --job-name=test
-#
+#SBATCH --output=res.txt
 #SBATCH --time=10:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
 
 srun hostname
-# srun sleep 60
+srun sleep 60
+
 echo "Current working directory: $PWD"
 ls -l
 cd /home/marcus.forst
