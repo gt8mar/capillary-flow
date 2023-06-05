8bf29672c6e4d06b5c6c65afe57c3f6691d0ad16
gt8mar <gt8mar@yahoo.com>
2023-06-05 16:59:04 -0700

    added functions to clean inputs from bash.

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -8,11 +8,41 @@ By: Marcus Forst
 """
 
 import time
-import os, sys
+import os, sys, re
 from src import write_background_file
 # from src import segment
 
+
 SET = "set_01"
+
+def clean_string(string="(21', '22', '23', '24', '25)\r"):
+    """
+    This function takes a string of numbers and returns a list of numbers.
+
+    Args:
+        string (str): a string of numbers
+
+    Returns:
+        list: a list of numbers    
+    """
+    input_string = string
+    cleaned_string = input_string.replace("(", "").replace(")", "").replace("'", "").replace("\r", "")
+    number_list = [int(num) for num in cleaned_string.split(", ")]
+    return number_list
+
+def extract_numbers_from_string(string):
+    """
+    This function takes a string of numbers and returns a list of numbers.
+
+    Args:
+        string (str): a string of numbers
+
+    Returns:
+        list: a list of numbers    
+    """
+    numbers = re.findall(r'\d+', string)
+    numbers = [int(num) for num in numbers]
+    return numbers
 
 def main():
     """
@@ -30,21 +60,22 @@ def main():
 
     """ Write Background """
     i = sys.argv[1]
+    print(i)
     ticks_total = time.time()
     participant = 'part' + str(i).zfill(2) 
-    date = os.listdir(os.path.join('/hpc/projects/capillary-flow/data', participant))
-    videos = os.listdir(os.path.join('/hpc/projects/capillary-flow/data', participant, date[0]))
-    for video in videos:
-        ticks = time.time()
-        path =  os.path.join('/hpc/projects/capillary-flow/data', participant, date[0], video)
-        write_background_file.main(path, color = True)
-        print(f'video {video}')
-        print(str(ticks-time.time()))
-    """ Segment capillaries using segment.py """
-    # TODO: make this work
-    # segment.main()
-    # find_centerline.py
-    # make_kymograph.py 
+    # date = os.listdir(os.path.join('/hpc/projects/capillary-flow/data', participant))
+    # videos = os.listdir(os.path.join('/hpc/projects/capillary-flow/data', participant, date[0]))
+    # for video in videos:
+    #     ticks = time.time()
+    #     path =  os.path.join('/hpc/projects/capillary-flow/data', participant, date[0], video)
+    #     write_background_file.main(path, color = True)
+    #     print(f'video {video}')
+    #     print(str(ticks-time.time()))
+    # """ Segment capillaries using segment.py """
+    # # TODO: make this work
+    # # segment.main()
+    # # find_centerline.py
+    # # make_kymograph.py 
 
     print(f'finished {participant} from the {date[0]}')
     print(str(ticks_total-time.time()))    
