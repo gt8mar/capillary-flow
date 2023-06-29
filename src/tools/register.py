9343f8a898a254df036be187701b26b056f4d403
gt8mar <gt8mar@yahoo.com>
2023-06-29 12:38:13 -0700

    made this function

--- src/tools/register.py
+++ src/tools/register.py
@@ -2,54 +2,71 @@
 Filename: register.py
 -------------------------------------------------------------
 This file 
-by: Gabby Rincon
+by: Gabby Rincon & ChatGPT
 """
 
 import os
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
 import cv2
-from skimage.registration import phase_cross_correlation
-from skimage import exposure, util
-from skimage.filters import gaussian
-
 
 def main():
-    image1 = cv2.imread("D:\\Marcus\\gabby test data\\part09\\230414\\vid1\\moco\\vid1_moco_0000.tif", cv2.COLOR_BGR2GRAY)
-    image2 = cv2.imread("D:\\Marcus\\gabby test data\\part09\\230414\\vid2\\moco\\vid2_moco_0000.tif", cv2.COLOR_BGR2GRAY)
+    source_img = cv2.imread("D:\\data_gabby\\230201\\vid7\\moco\\part_7_230201_7_0000.tif", cv2.COLOR_BGR2GRAY)
+    target_img = cv2.imread("D:\\data_gabby\\230201\\vid3\\moco\\part_7_230201_3_0000.tif", cv2.COLOR_BGR2GRAY)
 
-    #test with same image but shifted- does work
-    '''empty1 = np.empty_like(image1)
-    offset1 = empty1 + np.mean(image1)
-    offset_x = 10
-    offset_y = 50
-    height, width = image1.shape
-    for x in range(height):
-        for y in range(width):
-            if 0 <= x + offset_x < width and 0 <= y + offset_y < height:
-                offset1[y,x] = image1[y+offset_y,x+offset_x]'''
+    # Initialize the SIFT feature detector and descriptor
+    sift = cv2.SIFT_create()
 
-    #test with increased contrast- doesn't work
-    '''image1norm = util.img_as_float(image1)
-    image2norm = util.img_as_float(image2)
-    image1contrast = exposure.rescale_intensity(image1norm)
-    image2contrast = exposure.rescale_intensity(image2norm)'''
-    
-    #gaussians
-    image1hp = image1 - gaussian(image1)
-    image2hp = image2 - gaussian(image2)
-    #plt.imshow(image1hp)
-    #plt.show()
-    #cv2.imshow("2", image2hp)
+    # Detect keypoints and compute descriptors for the source and target images
+    keypoints1, descriptors1 = sift.detectAndCompute(source_img, None)
+    keypoints2, descriptors2 = sift.detectAndCompute(target_img, None)
 
-    #shift = phase_cross_correlation(image1contrast, image2contrast)[0]
-    shift = phase_cross_correlation(image1hp, image2hp)[0]
-    #shift = cv2.phaseCorrelate(np.float32(image1), np.float32(image2))
-    print(shift)
+    # Create a brute-force matcher
+    matcher = cv2.BFMatcher()
 
-""" 
+    # Match descriptors between the source and target images
+    matches = matcher.knnMatch(descriptors1, descriptors2, k=2)
+
+    # Apply Lowe's ratio test to filter good matches
+    good_matches = []
+    for m, n in matches:
+        if m.distance < 0.5 * n.distance:
+            good_matches.append(m)
+
+    # Calculate the translation parameters
+    dx_sum = 0.0
+    dy_sum = 0.0
+    num_matches = len(good_matches)
+
+    for match in good_matches:
+        print(match)
+        # Get the keypoints for the matched pair
+        kp1 = keypoints1[match.queryIdx]
+        kp2 = keypoints2[match.trainIdx]
+
+        # Calculate the translation between keypoints
+        dx = kp2.pt[0] - kp1.pt[0]
+        dy = kp2.pt[1] - kp1.pt[1]
+
+        dx_sum += dx
+        dy_sum += dy
+
+    # Calculate the average translation
+    dx_avg = dx_sum / num_matches
+    dy_avg = dy_sum / num_matches
+
+    # Calculate transformation matrix
+    transformation_matrix = np.array([[1, 0, dx_avg], [0, 1, dy_avg]], dtype=np.float32)
+
+    # Apply transformation
+    registered_img = cv2.warpAffine(source_img, transformation_matrix, (target_img.shape[1], target_img.shape[0]))
+
+    # Save registered image
+    dest_path = "D:\\data_gabby\\230201\\vid7\\moco registered\\part_7_230201_7_0000_reg2.tif"
+    cv2.imwrite(dest_path, registered_img)
+    print("saved")
+"""
 -----------------------------------------------------------------------------
 """
 # This provided line is required at the end of a Python file
