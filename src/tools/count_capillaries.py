9560b7b94d05f74ad9523a3af936c037b18daac4
gt8mar <gt8mar@yahoo.com>
2023-07-14 14:26:45 -0700

    file to count capillaries

---
+++ src/tools/count_capillaries.py
@@ -0,0 +1,24 @@
+"""
+File: count_capillaries.py
+-------------------------
+This program counts the maximum number of TIFF files in a 'kymo' folder.
+
+By: Marcus Forst
+"""
+
+import os
+
+def count_max_tiff_files(folder_path):
+    max_tiff_count = 0
+    for root, dirs, files in os.walk(folder_path):
+        if "kymo" in dirs:
+            kymo_folder = os.path.join(root, "kymo")
+            tiff_count = len([file for file in os.listdir(kymo_folder) if file.lower().endswith(".tiff") or file.lower().endswith(".tif")])
+            if tiff_count > max_tiff_count:
+                max_tiff_count = tiff_count
+    return max_tiff_count
+
+folder_path = "data"
+max_tiff_count = count_max_tiff_files(folder_path)
+
+print("Maximum number of TIFF files in a 'kymo' folder:", max_tiff_count)
