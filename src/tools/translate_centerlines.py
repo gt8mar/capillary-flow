43826dca5e103a061eae909b9ffdfa24ad2506e2
gt8mar <gt8mar@yahoo.com>
2023-07-24 16:35:53 -0700

    idk

--- src/tools/translate_centerlines.py
+++ src/tools/translate_centerlines.py
@@ -48,7 +48,7 @@ def translate_coords(coords_fp, sorted_coords_listdir, translations_csv, crops_c
     os.makedirs(translated_coords_fp, exist_ok=True)
     #apply translation
     for x in range(len(grouped_coords_listdir)):
-        dy = int(float(translations[x][0])) + int(float(crops[x][0]))
+        dy = int(float(translations[x][0])) - int(float(crops[x][0]))
         dx = int(float(translations[x][1])) - int(float(crops[x][3]))
         for file in grouped_coords_listdir[x]:
             with open(os.path.join(coords_fp, file), 'r') as orig_coords:
@@ -93,6 +93,11 @@ def rename_caps(coords_fp, individual_caps_fp):
     renamed_folder_fp = os.path.join(os.path.split(coords_fp)[0], "renamed")
     os.makedirs(renamed_folder_fp, exist_ok=True)
     for file in os.listdir(coords_fp):
+        
+        match = re.search(r'vid(\d{2})', file)
+        vidnum = match.group(1)
+        vids = [string for string in os.listdir(individual_caps_fp) if f"vid{vidnum}" in string]
+
         #get centerline midpoint x and y values
         with open(os.path.join(coords_fp, file), 'r') as coords:
             reader = csv.reader(coords)
@@ -100,45 +105,60 @@ def rename_caps(coords_fp, individual_caps_fp):
             midpoint_row = rows[len(rows) // 2]
             midpoint_x = midpoint_row[0]
             midpoint_y = midpoint_row[1]
-
-        """#check which capillary this file corresponds to
-        for x in range(len(os.listdir(individual_caps_fp))):
-            image_array = cv2.imread(os.path.join(individual_caps_fp, os.listdir(individual_caps_fp)[x]))
-            gray_image = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
-            midpoint_x_int = int(float(midpoint_x))
-            midpoint_y_int = int(float(midpoint_y))
-            #if cap contains midpoint x, y
-            if gray_image[midpoint_x_int][midpoint_y_int] > 0:
-                new_csv_filename = file[:-6] + "cap_" + ("%02d" % x) + ".csv"
-                shutil.copy(os.path.join(coords_fp, file), os.path.join(renamed_folder_fp, new_csv_filename))
-                break"""
-        
-        for cap in os.listdir(individual_caps_fp):
-            image_array = cv2.imread(os.path.join(individual_caps_fp, cap))
-            gray_image = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
-            midpoint_x_int = int(float(midpoint_x))
-            midpoint_y_int = int(float(midpoint_y))
-            if gray_image[midpoint_x_int][midpoint_y_int] > 0:
-                new_csv_filename = file[:-6] + cap[-11:-4] + ".csv"
-                shutil.copy(os.path.join(coords_fp, file), os.path.join(renamed_folder_fp, new_csv_filename))
-                break
+            
+            for vid in vids:
+                image_array = cv2.imread(os.path.join(individual_caps_fp, vid))
+                gray_image = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
+                midpoint_x_int = int(float(midpoint_x))
+                midpoint_y_int = int(float(midpoint_y))
+                num_matches = 0
+                if gray_image[midpoint_x_int][midpoint_y_int] > 0:
+                    for row in rows:
+                        if gray_image[int(float(row[0]))][int(float(row[1]))] > 0:
+                            num_matches += 1
+                    if num_matches > 0.8*len(rows):
+                        new_csv_filename = file[:-6] + vid[-11:-4] + ".csv"
+                        shutil.copy(os.path.join(coords_fp, file), os.path.join(renamed_folder_fp, new_csv_filename))
+                        break
 
     return renamed_folder_fp
 
 #TEMP
-def show_centerlines(projected_caps_fp, translated_coords_fp):
+def show_centerlines(projected_caps_fp, coords_fp, individual_caps_fp):
     maxproj = np.zeros([1080,1440,3])
     for cap in os.listdir(projected_caps_fp):
         maxproj += cv2.imread(os.path.join(projected_caps_fp, cap))
 
-    for file in os.listdir(translated_coords_fp):
-        with open(os.path.join(translated_coords_fp, file), 'r') as translated_coords:
-            reader = csv.reader(translated_coords)
+    for file in os.listdir(coords_fp):
+        with open(os.path.join(coords_fp, file), 'r') as coords:
+            reader = csv.reader(coords)
             rows = list(reader)
             for row in rows:
                 maxproj[int(float(row[0]))][int(float(row[1]))] = [255, 0, 0]
     cv2.imshow(str(file), maxproj)
     cv2.waitKey(0)
+
+    for file in os.listdir(coords_fp):
+
+        match1 = re.search(r'vid(\d{2})', file)
+        vidnum = match1.group(1)
+
+        match2 = re.search(r'cap_(.{3})', file)
+        capnum = match2.group(1)
+
+        cap_img = None
+        for cap in os.listdir(individual_caps_fp):
+            if cap.__contains__("vid" + vidnum) and cap.__contains__("cap_" + capnum):
+                cap_img = cv2.imread(os.path.join(individual_caps_fp, cap))
+                break
+
+        with open(os.path.join(coords_fp, file), 'r') as coords:
+            reader = csv.reader(coords)
+            rows = list(reader)
+            for row in rows:
+                cap_img[int(float(row[0]))][int(float(row[1]))] = [255, 0, 0]
+        cv2.imshow(str(file), cap_img)
+        cv2.waitKey(0)  
 
 """def plot_radii(renamed_coords_fp):
     #group coords files by cap
@@ -200,19 +220,20 @@ def show_centerlines(projected_caps_fp, translated_coords_fp):
 
 
 def main():
-    coords_fp = "E:\\Marcus\\gabby test data\\part11_centerlines_test\\coords"
+    coords_fp = "E:\\Marcus\\gabby test data\\part11_centerlines_test\\coords2"
     registered_folder = "E:\\Marcus\\gabby test data\\part11_segmented\\registered"
 
     sorted_coords_listdir = sorted_seg_listdir = sorted(filter(lambda x: os.path.isfile(os.path.join(coords_fp, x)), os.listdir(coords_fp))) #sort numerically
     translations_csv = os.path.join(registered_folder, "translations.csv")
     projected_caps_fp = os.path.join(registered_folder, "proj_caps")
     crops_csv = os.path.join(registered_folder, "crop_values.csv")
-    individual_caps = os.path.join(registered_folder, "individual_caps")
+    individual_caps_fp = os.path.join(registered_folder, "individual_caps")
 
     translated_coords_fp = translate_coords(coords_fp, sorted_coords_listdir, translations_csv, crops_csv)
-    #show_centerlines(projected_caps_fp, translated_coords_fp)
+    
     #renamed_coords_fp = rename_caps(translated_coords_fp, projected_caps_fp)
-    renamed_coords_fp = rename_caps(translated_coords_fp, individual_caps)
+    renamed_coords_fp = rename_caps(translated_coords_fp, individual_caps_fp)
+    show_centerlines(projected_caps_fp, renamed_coords_fp, individual_caps_fp)
     #plot_radii(renamed_coords_fp)
 
     
