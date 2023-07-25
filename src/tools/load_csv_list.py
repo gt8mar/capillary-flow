1f4d59d9db6244ccec17161e64e1a9a68ef8d9f2
gt8mar <gt8mar@yahoo.com>
2023-07-24 21:35:35 -0700

    Added comments

--- src/tools/load_csv_list.py
+++ src/tools/load_csv_list.py
@@ -1,8 +1,24 @@
+"""
+Filename: load_csv_list.py
+------------------------------------------------------
+This program loads csv files from a folder into a list.
+
+By: Marcus Forst
+"""
+
 import os
 import numpy as np
 
 def load_csv_list(path, dtype = int):
-    """ Loads csvs from a folder into a list """
+    """ 
+    Loads csv files from a folder into a list 
+
+    Args:
+        path (str): the path to the folder to be searched
+
+    Returns:
+        list: a list of csv files (dtype= 2D np.array) in the given path
+    """
     csv_names = [file for file in os.listdir(path) if file.endswith(".csv")]
     csv_list = []
     for name in csv_names:
