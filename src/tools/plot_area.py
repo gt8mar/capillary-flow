c070770a759b1a8949bb2c9aefab49b1ac2990fe
gt8mar <gt8mar@yahoo.com>
2023-07-28 17:40:45 -0700

    boxplots

--- src/tools/plot_area.py
+++ src/tools/plot_area.py
@@ -13,6 +13,8 @@ import cv2
 import csv
 import re
 import matplotlib.colors as mcolors
+from collections import defaultdict
+
 
 def exclude_fragmented(caps_fp):
     caps_listdir = []
@@ -49,7 +51,7 @@ def group_by_cap(plotinfo):
     grouped_caps = {}
     
     for entry in plotinfo:
-        capnum = entry[2]
+        capnum = int(entry[2])
         if capnum in grouped_caps:
             grouped_caps[capnum].append(entry)
         else:
@@ -58,48 +60,20 @@ def group_by_cap(plotinfo):
     result_list = list(grouped_caps.values())
     return result_list   
 
-"""def plot(plotinfo):
-    y_values = [point[0] for point in plotinfo]
-    x_values = [float(point[1]) for point in plotinfo] 
-    legend_labels = [point[2] for point in plotinfo]
-    unique_legend_labels = list(set(legend_labels))
-    unique_legend_labels.sort() 
-    for label in unique_legend_labels:
-        mask = [l == label for l in legend_labels]
-        plt.scatter(np.array(x_values)[mask], np.array(y_values)[mask], label=label)
-    plt.xlabel('Pressure (psi)')
-    plt.ylabel('Area/Length')
-    plt.legend(title='Legend') 
-    plt.show()"""
+def group_by_vidnum(plotinfo):
+    grouped_caps = {}
+    
+    for entry in plotinfo:
+        vidnum = int(entry[3])
+        if vidnum in grouped_caps:
+            grouped_caps[vidnum].append(entry)
+        else:
+            grouped_caps[vidnum] = [entry]
+    
+    result_list = list(grouped_caps.values())
+    return result_list   
 
-"""def subplot(plotinfo, title):
-    for point in plotinfo:
-        point.append("Blue")
-    for x in range(len(plotinfo)-1):
-        if float(plotinfo[x][1]) > float(plotinfo[x+1][1]):
-            plotinfo[x+1][4] = "Red"
-    sorted_plotinfo = sorted(plotinfo, key=lambda x: x[1])
-    x_line = [entry[1] for entry in plotinfo]
-    y_line = [entry[0] for entry in plotinfo]
-
-    # Create a figure and an array of subplots
-    fig, ax = plt.subplots()
-
-    if "Red" in [entry[4] for entry in sorted_plotinfo]:
-        cmap = mcolors.LinearSegmentedColormap.from_list('custom_cmap', ['blue', 'red'], N=len(x_line) - 1)
-        for i in range(len(x_line) - 1):
-            ax.plot([x_line[i], x_line[i + 1]], [y_line[i], y_line[i + 1]], c=cmap(i))
-    else:
-        ax.plot(x_line, y_line)
-
-    ax.scatter([entry[1] for entry in sorted_plotinfo], [entry[0] for entry in sorted_plotinfo], c=[entry[4] for entry in sorted_plotinfo])
-    ax.set_xlabel('Pressure (psi)')
-    ax.set_ylabel('Area/Length')
-    ax.set_title(title)
-
-    plt.show()"""
-
-def plot_subplots(plotinfo):
+def subplots(plotinfo):
     grouped_plotinfo = group_by_cap(plotinfo)
     num_plots = len(grouped_plotinfo)
     num_cols = 3
@@ -123,13 +97,13 @@ def plot_subplots(plotinfo):
 
             sorted_cap = sorted(cap, key=lambda x: x[1])
 
-            x_scatter = [float(entry[1]) for entry in sorted_cap]  # Convert to floats
+            x_scatter = [float(entry[1]) for entry in sorted_cap]  
             y_scatter = [entry[0] for entry in sorted_cap]
             colors = [entry[4] for entry in sorted_cap]
 
             ax.scatter(x_scatter, y_scatter, c=colors)
 
-            x_line = [float(entry[1]) for entry in cap]  # Convert to floats
+            x_line = [float(entry[1]) for entry in cap]  
             y_line = [entry[0] for entry in cap]
             if "Red" in [entry[4] for entry in sorted_cap]:
                 cmap = mcolors.LinearSegmentedColormap.from_list('custom_cmap', ['blue', 'red'], N=len(x_line) - 1)
@@ -156,91 +130,54 @@ def plot_subplots(plotinfo):
     plt.tight_layout()
     plt.show()
 
-#not using this anymore
-#only can compare areas between one capillary
-def plot_area(caps_fp, centerlines_fp, metadata_fp):
-    caps_listdir_nofrag = exclude_fragmented(caps_fp)
+
+def boxplot(plotinfo):
+    grouped_by_vidnum = group_by_vidnum(plotinfo)
+    xvals = []
+    yvals = []
+    labels = []
+
+    for vidnum in grouped_by_vidnum:
+        labels.append(vidnum[0][1])
+        xvals.append(float(vidnum[0][1]))
+        yval = []
+        for point in vidnum:
+            yval.append(point[0])
+        yvals.append(yval)
+
+    xvals_up = xvals
+    xvals_down = []
+    yvals_up = yvals
+    yvals_down = []
+    for i in range(1,len(grouped_by_vidnum)):
+        if grouped_by_vidnum[i][0][1] < grouped_by_vidnum[i-1][0][1]:
+            xvals_up = xvals[:i]
+            xvals_down = xvals[i:]
+            yvals_up = yvals[:i]
+            yvals_down = yvals[i:]
+            break
+
+    fig, ax = plt.subplots(1, 2, figsize=(15, 5), sharey = True)
     
-    plotinfo = []
+    for i in range(len(yvals_up)):
+        ax[0].scatter([xvals_up[i] for _ in range(len(yvals_up[i]))], yvals_up[i], c="Black")
+    for i in range(len(yvals_down)):
+        ax[1].scatter([xvals_down[i] for _ in range(len(yvals_down[i]))], yvals_down[i], c="Black")
     
-    #group coords files by cap
-    pattern = r"cap_(\d{2})"
-    groups = {}
-    for string in caps_listdir_nofrag:
-        match = re.search(pattern, string)
-        if match:
-            number = match.group(1)
-            groups.setdefault(number, []).append(string)
-    grouped_caps_listdir = list(groups.values())
+    ax[0].boxplot(yvals_up, positions=xvals_up)
+    ax[1].boxplot(yvals_down, positions=xvals_down)
+    print(xvals_down)
 
-    for cap in grouped_caps_listdir:
-        shortest_length = np.inf
-        cap_index = 0
-        counter = 0
+    #ax.legend()
+    ax[0].set_xlabel('Pressure (psi)')
+    ax[0].set_ylabel('Area/Length')
+    ax[0].set_title('Up')
 
-        #find shortest length
-        for vid in cap: 
-            #find vid & cap num
-            vmatch = re.search(r'vid(\d{2})', vid)
-            vidnum = vmatch.group(1)
-            cmatch = re.search(r'cap_(.{3})', vid)
-            capnum = cmatch.group(1)
-
-            #find centerline file that corresponds to vidnum
-            centerline_file = ""
-            for ctrline in os.listdir(centerlines_fp):
-                if vidnum in ctrline and capnum in ctrline:
-                    centerline_file = os.path.join(centerlines_fp, ctrline)
-                    break
-            if centerline_file == "": continue #no centerline file for this vid
-
-            #find shortest capillary length
-            with open(centerline_file, 'r') as caps:
-                reader = csv.reader(caps)
-                rows = list(reader)
-                top = float(rows[-1][0])
-                bottom = float(max(rows, key=lambda row: float(row[0]))[0])
-                if top == bottom:
-                    top = float(rows[0][0])
-                length = int(bottom - top)
-                if length < shortest_length:
-                    shortest_length = length
-                    cap_index = counter
-                counter += 1
-
-        #get areas & pressure
-        for vid in cap: 
-            #find area
-            vid_img = cv2.imread(os.path.join(caps_fp, vid), cv2.IMREAD_GRAYSCALE)
-            area = 0
-            found_bottom = False
-            for row in range(vid_img.shape[0] - 1, 0, -1):
-                for col in range(vid_img.shape[1]):
-                    if vid_img[row][col] > 1:
-                        found_bottom = True
-                        for r in range(row, row - shortest_length, -1):
-                            for c in range(vid_img.shape[1]):
-                                if vid_img[r][c] > 1:
-                                    area += 1
-                    if found_bottom: break
-                if found_bottom: break
-
-            #find pressure
-            vmatch = re.search(r'vid(\d{2})', vid)
-            vidnum = vmatch.group(1)
-            with open(metadata_fp, 'r') as metadata:
-                reader = csv.reader(metadata)
-                rows = list(reader)
-                for row in rows:
-                    if vidnum in str(row[3]):
-                        pressure = str(row[5])
-                        break
-
-            cmatch = re.search(r'cap_(.{3})', vid)
-            capnum = cmatch.group(1)
-            plotinfo.append([area, pressure, "cap" + capnum[:-1]])
-
-    #plot areas
+    ax[1].set_xlabel('Pressure (psi)')
+    ax[1].set_ylabel('Area/Length')
+    ax[1].set_title('Down')
+    plt.show()
+    
 
 def plot_area_by_length(caps_fp, centerlines_fp, metadata_fp):
     caps_listdir_nofrag = exclude_fragmented(caps_fp)
@@ -284,23 +221,18 @@ def plot_area_by_length(caps_fp, centerlines_fp, metadata_fp):
                         pressure = str(row[5])
                         break
 
-        plotinfo.append([area/length, pressure, "cap_" + capnum, vidnum])
+        plotinfo.append([area/length, pressure, capnum, vidnum])
 
-    #split plotinfo
-    """grouped_plotinfo = group_by_cap(plotinfo)
-    for cap in grouped_plotinfo:
-        plot(cap, cap[0][2])
-    plot(plotinfo)"""
-    plot_subplots(plotinfo)
-
+    #subplots(plotinfo)
+    boxplot(plotinfo)
     
 def main():
     caps_fp = "E:\\Marcus\\gabby test data\\part11_segmented\\registered\\individual_caps"
     centerlines_fp = "E:\\Marcus\\gabby test data\\part11_centerlines_test\\coords2\\renamed"
     metadata_fp = "E:\\Marcus\\gabby test data\\part11\\part11_230427.xlsx - Sheet1.csv"
 
-    #plot_area(caps_fp, centerlines_fp, metadata_fp)
     plot_area_by_length(caps_fp, centerlines_fp, metadata_fp)
+    
 
 """
 -----------------------------------------------------------------------------
