c8f2d8a9c4f27bef4f1e92c092692c0a01d576b2
gt8mar <gt8mar@yahoo.com>
2023-08-14 16:30:02 -0700

    size pipeline

---
+++ scripts/pipeline_size.py
@@ -0,0 +1,42 @@
+from src.tools import align_segmented
+from src.tools import group_caps
+from src.tools import translate_centerlines
+from src.tools import plot_area
+from src.tools import plot_size_slopes
+from src.tools.find_earliest_date_dir import find_earliest_date_dir
+import os
+import sys
+import time
+
+def main():
+    # Participant number is passed as an argument
+    i = sys.argv[1]
+    print(f"begin size_pipeline for participant {i}")
+    ticks_total = time.time()
+    participant = 'part' + str(i).zfill(2) 
+
+    # Load the date and video numbers
+    date = find_earliest_date_dir(os.path.join('/hpc/projects/capillary-flow/data', participant))
+    locations = os.listdir(os.path.join('/hpc/projects/capillary-flow/data', participant, date))
+    for location in locations:
+        print(f"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+        print(f"beginning size for location {location}")
+        print(f"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+        ticks = time.time()
+        location_path =  os.path.join('/hpc/projects/capillary-flow/data', participant, date, location)
+        align_segmented.align_segmented(location_path)
+        group_caps.main(location_path)
+        translate_centerlines.main(location_path)
+        plot_area.main(location_path)
+        plot_size_slopes.main(location_path)
+    
+
+if __name__ == "__main__":
+    print("Run full pipeline")
+    print("-------------------------------------")
+    ticks_first = time.time()
+    ticks = time.time()
+    main()  
+
+    print("-------------------------------------")
+    print("Total Pipeline Runtime: " + str(time.time() - ticks_first))
