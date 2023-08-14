a023aa0215a5091ff7001fa0a1ba366c4e95fd16
gt8mar <gt8mar@yahoo.com>
2023-08-14 16:30:13 -0700

    size bash

---
+++ scripts/size_submit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=capSize
+#SBATCH --time=10:00:00
+#SBATCH --gpus=2
+#SBATCH --partition=gpu
+#SBATCH --mem=150G
+#SBATCH --output=cap_size_array_%A-%a.out
+#SBATCH --array=9-20
+
+cd /hpc/projects/capillary-flow/scripts
+module load anaconda
+conda activate capillary-flow
+echo "size: participant $SLURM_ARRAY_TASK_ID"
+srun python pipeline_size.py ${SLURM_ARRAY_TASK_ID}
+echo "completed size part$SLURM_ARRAY_TASK_ID"
+exit
