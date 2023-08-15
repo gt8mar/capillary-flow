94593aea73207e8549949e76d42f34511406e174
gt8mar <gt8mar@yahoo.com>
2023-08-15 15:21:13 -0700

    checks to see if seg and vids are equal

--- src/tools/count_seg_moco.py
+++ src/tools/count_seg_moco.py
@@ -9,44 +9,61 @@ By: Marcus Forst
 
 import os
 import pandas as pd
-from src.tools.find_earliest_date_dir import find_earliest_date_dir
+from find_earliest_date_dir import find_earliest_date_dir
 
 
 
-def count_seg_moco(dir_path, data):
+def count_seg_moco(dir_path):
     num_seg = 0
     num_moco = 0
 
-    for item in os.listdir(dir_path):
-        item_path = os.path.join(dir_path, item)
+
+
+    # for item in os.listdir(dir_path):
+    #     item_path = os.path.join(dir_path, item)
         
-        if os.path.isdir(item_path):
-            if item.startswith('loc'):
-                for sub_item in os.listdir(item_path):
-                    sub_item_path = os.path.join(item_path, sub_item)
-                    if sub_item.endswith('seg.png') and os.path.isfile(sub_item_path):
-                        num_seg += 1
-                    elif sub_item == 'moco' and os.path.isdir(sub_item_path):
-                        num_moco += 1
+    #     if os.path.isdir(item_path):
+    #         if item.startswith('loc'):
+    #             for sub_item in os.listdir(item_path):
+    #                 sub_item_path = os.path.join(item_path, sub_item)
+    #                 if sub_item.endswith('seg.png') and os.path.isfile(sub_item_path):
+    #                     num_seg += 1
+    #                 elif sub_item == 'moco' and os.path.isdir(sub_item_path):
+    #                     num_moco += 1
 
-            count_seg_moco(item_path)
+    #         count_seg_moco(item_path, data)
 
-    if num_seg > 0 or num_moco > 0:
-        data['location'].append(dir_path)
-        data['num_seg'].append(num_seg)
-        data['num_moco'].append(num_moco)
+    # if num_seg > 0 or num_moco > 0:
+    #     data['location'].append(dir_path)
+    #     data['num_seg'].append(num_seg)
+    #     data['num_moco'].append(num_moco)
 
+def main():
+    root_directory = '/hpc/projects/capillary-flow/data'
+    for i in range(9, 21):
+        participant = 'part' + str(i).zfill(2)
+        date = find_earliest_date_dir(os.path.join(root_directory, participant))
+        df = pd.DataFrame(columns=['location', 'num_vids', 'num_seg'])
+        for location in os.listdir(os.path.join(root_directory, participant, date)):
+            print(f'participant: {participant}, date: {date}, location: {location}')
+            vids = os.listdir(os.path.join(root_directory, participant, date, location, 'vids'))
+            if len(vids) == 0:
+                print(f'participant: {participant}, date: {date}, location: {location} has no videos')
+            num_vids = len(vids)
+            segs = os.listdir(os.path.join(root_directory, participant, date, location, 'segmented'))
+            num_seg = len([seg for seg in segs if seg.endswith('seg.png')])
+            row = {'location': location, 'num_vids': num_vids, 'num_seg': num_seg}
+            df = pd.concat([df, pd.DataFrame(row, index=[0])])
+        print(df)
+            
+            
+
+            # data = count_seg_moco(dir_path)
+            # df = pd.DataFrame(data)
+            # print(df)
 
 if __name__ == '__main__':
-    root_directory = '/hpc/projects/capillary-flow/data'
-    for participant in os.path.listdir(root_directory):
-        date = find_earliest_date_dir(os.path.join(root_directory, participant))
-        for location in os.listdir(os.path.join(root_directory, participant, date)):
-            dir_path = os.path.join(root_directory, participant, date, location)
-            data = {'location': [], 'num_seg': [], 'num_moco': []}
-            count_seg_moco(dir_path, data)
-            df = pd.DataFrame(data)
-            print(df)
+    main()
 
         
 
