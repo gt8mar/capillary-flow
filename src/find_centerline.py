c8c56d2b5cb0ff87d793b3af72177aed41f36949
gt8mar <gt8mar@yahoo.com>
2023-08-17 10:35:00 -0700

    enumerate capillaries 2

--- src/find_centerline.py
+++ src/find_centerline.py
@@ -191,7 +191,7 @@ def main(path = "F:\\Marcus\\data\\part09\\230414\\loc01",
 
             # Make a numpy array of images with isolated capillaries. The mean/sum of this is segmented_2D.
             # TODO: fix this horrible plotting
-            contours = enumerate_capillaries(segmented, verbose=False, write=write, write_path = os.path.join(output_folder, 'images', cap_map_filename))
+            contours = enumerate_capillaries2(segmented)    # , verbose=False, write=write, write_path = os.path.join(output_folder, 'images', cap_map_filename)
             
             if write:
                 # save segmented_2D
