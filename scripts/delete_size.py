fbc383071ea27cf945837b62966021d13f6b8f1e
gt8mar <gt8mar@yahoo.com>
2023-08-30 11:20:51 -0700

    fixed

--- scripts/delete_size.py
+++ scripts/delete_size.py
@@ -19,21 +19,15 @@ def delete_centerlines_files(source_folder):
     for i in range(9,21):
         participant = 'part' + str(i).zfill(2)
         date = find_earliest_date_dir(os.path.join(source_folder, participant))
+        #remove size folder in locations
         for location in os.listdir(os.path.join(source_folder, participant, date)):
-            # delete all files and folders in size
-            folder_list = []
-            file_list = []
-            for file in os.listdir(os.path.join(source_folder, participant, date, location, 'size')):
-                item_path = os.path.join(source_folder, participant, date, location, 'size', file)
-                if os.path.isdir(os.path.join(source_folder, participant, date, location, 'size', file)):
-                    folder_list.append(item_path)
-                else:
-                    file_list.append(item_path)     
-            for file in file_list:
-                os.remove(os.path.join(source_folder, participant, date, location, 'size', file))
-            for folder in folder_list:
-                shutil.rmtree(os.path.join(source_folder, participant, date, location, 'size', folder))            
-    return 0
+            size_fp = os.path.join(source_folder, participant, date, location, 'size')
+            if os.path.exists(size_fp):
+                shutil.rmtree(size_fp)
+        #remove size folder in results
+        size_results_fp = '/hpc/projects/capillary-flow/results/size'
+        if os.path.exists(size_results_fp):
+            shutil.rmtree(size_results_fp)
 
 if __name__ == "__main__":
     source_folder = "/hpc/projects/capillary-flow/data"
