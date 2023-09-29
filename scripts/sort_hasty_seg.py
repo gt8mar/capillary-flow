fb4c5e6b97c7ac96f8b48cb57ac3bd349277c023
gt8mar <gt8mar@yahoo.com>
2023-09-29 16:34:35 -0700

    script to sort hasty seg files into location folders

---
+++ scripts/sort_hasty_seg.py
@@ -0,0 +1,44 @@
+"""
+Filename: sort_hasty_seg.py
+---------------------------
+This module contains a function that sorts the hasty segmentations into the
+correct location folders.
+"""
+
+import os, platform
+import shutil
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from src.tools.parse_filename import parse_filename
+
+def main():
+    if platform.system() == 'Windows':
+        seg_path = 'C:\\Users\\gt8mar\\capillary-flow\\results\\segmented\\hasty\\230626'
+    else:
+        seg_path = '/hpc/projects/capillary-flow/results/segmented/hasty/230626'
+
+    # get all files in the directory
+    files = os.listdir(seg_path)
+    # replace _contrast and _background
+    # files = [file.replace('_contrast', '').replace('_background', '').replace('set01_','').replace('set_01_', '') for file in files]
+
+    for file in files:
+        participant, date, location, video, file_prefix = parse_filename(file)
+        filename = file_prefix + '_seg.png'
+        # create the new path
+        if platform.system() == 'Windows':
+            new_path = f'C:\\Users\\gt8mar\\capillary-flow\\data\\{participant}\\{date}\\{location}\\segmented\\hasty\\' 
+            os.makedirs(new_path, exist_ok=True)
+        else:
+            new_path = f'/hpc/projects/capillary-flow/data/{participant}/{date}/{location}/segmented/hasty/'
+            os.makedirs(new_path, exist_ok=True)
+        # copy the file to the new path
+        shutil.copy(os.path.join(seg_path, file), os.path.join(new_path, filename))
+
+    print('Done!')
+
+if __name__ == "__main__":
+    main()
+    
+
