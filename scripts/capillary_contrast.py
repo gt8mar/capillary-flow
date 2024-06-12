ada3cacf3f91e43559e5ca6cc2dfed4e4c2b6bfb
gt8mar <gt8mar@yahoo.com>
2024-06-12 11:15:49 -0700

    added some contrast functions

--- scripts/capillary_contrast.py
+++ scripts/capillary_contrast.py
@@ -45,6 +45,9 @@ def main(input_folder, output_folder):
         input_path = os.path.join(input_folder, filename)
         output_path = os.path.join(output_folder, 'processed_' + filename)
         first_fame_contrast = cv2.equalizeHist(image)
+        
+        # min_, max_ = calculate_contrast_limits(first_image, first_image.dtype)
+        # processed_image = apply_contrast(image, min_, max_)
 
 
 
@@ -83,7 +86,72 @@ def main(input_folder, output_folder):
 
 
 
+"""
+Helpful functions
+-----------------
+"""
+def calculate_contrast_limits(image, image_type):
+    """Calculate the histogram-based contrast limits of an image.
 
+    Args:
+        image (ndarray): The image array in grayscale.
+        image_type (dtype): The data type of the image (e.g., np.uint8).
+
+    Returns:
+        tuple: A tuple containing the minimum and maximum values for contrast adjustment.
+
+    Raises:
+        NotImplementedError: If the image type is not supported.
+    """
+    # Set the histogram parameters based on the image data type
+    if image_type == np.uint8:
+        hist_min = 0
+        hist_max = 256
+    elif image_type in (np.uint16, np.int32):
+        hist_min = np.min(image)
+        hist_max = np.max(image)
+    else:
+        raise NotImplementedError(f"Not implemented for dtype {image_type}")
+
+    # Compute histogram
+    histogram, bin_edges = np.histogram(image, bins=256, range=(hist_min, hist_max))
+    bin_size = (hist_max - hist_min) / 256
+
+    # Calculate the thresholds
+    pixel_count = image.size
+    limit = pixel_count / 10
+    auto_threshold = max(5000, pixel_count / 1000)
+    threshold = int(pixel_count / auto_threshold)
+
+    # Find the minimum bin with count above the threshold
+    hmin = next((i for i, count in enumerate(histogram) if count > threshold and count > limit), 0)
+
+    # Find the maximum bin with count above the threshold
+    hmax = next((i for i in reversed(range(256)) if histogram[i] > threshold and histogram[i] > limit), 255)
+
+    # Convert histogram bins to actual pixel values
+    min_ = hist_min + hmin * bin_size
+    max_ = hist_min + hmax * bin_size
+
+    # Handle edge cases where no valid range is found
+    if hmax <= hmin:
+        min_ = hist_min
+        max_ = hist_max
+
+    return min_, max_
+
+def apply_contrast(image, min_, max_):
+    """Apply contrast stretching to an image using specified min and max values.
+
+    Args:
+        image (ndarray): The original image array.
+        min_ (float): The minimum pixel value for contrast stretching.
+        max_ (float): The maximum pixel value for contrast stretching.
+
+    Returns:
+        ndarray: The contrast-stretched image.
+    """
+    return np.clip((image - min_) / (max_ - min_) * 255, 0, 255).astype(np.uint8)
 
 
 # """
