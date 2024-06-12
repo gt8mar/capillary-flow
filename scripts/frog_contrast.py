aa4341ff45fe0f5394596f6c4e104886f92800d9
gt8mar <gt8mar@yahoo.com>
2024-06-12 11:10:30 -0700

    added results saving

--- scripts/frog_contrast.py
+++ scripts/frog_contrast.py
@@ -12,23 +12,22 @@ import time
 
 FOLDER = "C:\\Users\\ejerison\\capillary-flow\\frog\\results\\stdevs"
 OUTPUT_PATH = "C:\\Users\\ejerison\\capillary-flow\\frog\\results\\stdevs-contrasted"
+OUTPUT_PATH_CLAHE = "C:\\Users\\ejerison\\capillary-flow\\frog\\results\\stdevs-contrasted-clahe"
 os.makedirs(OUTPUT_PATH, exist_ok= True)
+os.makedirs(OUTPUT_PATH_CLAHE, exist_ok= True)
 
 def main(method = "hist"):
     filenames = os.listdir(FOLDER)
-    print(filenames)
+    # print(filenames)
     for filename in filenames:
         file_image = cv2.imread(os.path.join(FOLDER, filename), cv2.IMREAD_GRAYSCALE)
         if method == "hist":
             file_image = cv2.equalizeHist(file_image)
+            cv2.imwrite(os.path.join(OUTPUT_PATH,filename), file_image)
         else:
-            clahe = cv2.create_CLAHE(cliplimit = 2.0, tileGRIDSIZE = (8,8))
+            clahe = cv2.createCLAHE(clipLimit = 2.0, tileGridSize = (8,8))
             file_image = clahe.apply(file_image)
-        cv2.imwrite(OUTPUT_PATH, file_image)
-
-
-
-
+            cv2.imwrite(os.path.join(OUTPUT_PATH_CLAHE, filename), file_image)
 
 
 """
@@ -38,6 +37,6 @@ def main(method = "hist"):
 # to call the main() function.
 if __name__ == "__main__":
     ticks = time.time()
-    main()
+    main(method = 'banana')
     print("--------------------")
     print("Runtime: " + str(time.time() - ticks))
