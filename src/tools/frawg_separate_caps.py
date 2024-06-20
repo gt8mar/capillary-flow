7a96d09e72f21298588c8264f484689557d54c72
gt8mar <gt8mar@yahoo.com>
2024-06-20 11:45:53 -0700

    fixed

--- src/tools/frawg_separate_caps.py
+++ src/tools/frawg_separate_caps.py
@@ -2,6 +2,7 @@ import time
 import os
 import cv2
 from enumerate_capillaries2 import find_connected_components
+import csv
 
 def main(path):
     segmented_folder = os.path.join(path, 'segmented')
@@ -12,10 +13,21 @@ def main(path):
             file_image = cv2.imread(os.path.join(segmented_folder, file), cv2.IMREAD_GRAYSCALE)
             caps = find_connected_components(file_image)
             for i, cap in enumerate(caps):
-                cv2.imwrite(os.path.join(output_folder, file[4:-4] + '_' + str(i).zfill(2) + '.png'), cap)
+                file = file.replace('SD_', '')
+                file = file.replace('.png', '')
+                cv2.imwrite(os.path.join(output_folder, file + '_' + str(i).zfill(2) + '.png'), cap)
+
+    # save all file names to csv
+    csv_file = os.path.join(output_folder, 'cap_names.csv')
+    with open(csv_file, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        for file in os.listdir(output_folder):
+            if file.endswith('.png'):
+                file = file.strip('.png')
+                csvwriter.writerow([file])
 
 if __name__ == "__main__":
     ticks = time.time()
-    main(path = 'E:\\frawg\\gabbyanalysis')
+    main(path = 'E:\\frog\\24-2-14 WkSl\\Frog4\\Right')
     print("--------------------")
     print("Runtime: " + str(time.time() - ticks))
