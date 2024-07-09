1fb49d08f8d0f987fb06f2adf11911d091e9bb09
gt8mar <gt8mar@yahoo.com>
2024-07-08 21:40:08 -0700

    added slow velocities, saving, going back, going forward.

--- scripts/gui_kymos.py
+++ scripts/gui_kymos.py
@@ -1,30 +1,34 @@
 import os
 import pandas as pd
-import numpy as np
 import tkinter as tk
 from PIL import Image, ImageTk
 import cv2
 
 class KymographClassifier:
-    def __init__(self, image_dir, metadata_dir, csv_path):
+    def __init__(self, image_dir, metadata_dir, csv_path, output_csv_path):
         self.image_dir = image_dir
         self.metadata_dir = metadata_dir
         self.csv_path = csv_path
+        self.output_csv_path = output_csv_path
         self.df = pd.read_csv(csv_path)
         self.prepare_data()
         self.index = 0
         self.inverted_slope = False
-        self.high_velocities = [10, 750, 1000, 1500, 2000, 3000] 
-        self.current_velocity_index = 0 
+        self.high_velocities = [10, 500, 600, 750, 1000, 1500, 2000, 3000, 4000]  # Original velocities
+        self.additional_velocities = [10, 20, 35, 50, 75, 110, 160, 220, 300, 400]  # Additional velocities with Shift key
+        self.original_velocity = None
+        self.current_velocity_index = 0
+        self.use_additional_velocities = False  # Toggle between high and additional velocities
+        self.initialize_output_csv()
         self.setup_gui()
 
     def prepare_data(self):
-        # List all TIFF files in the directory
         tiff_files = {file for file in os.listdir(self.image_dir) if file.lower().endswith(('.tif', '.tiff'))}
-        # Add full image paths to the DataFrame and filter by existing files
         self.df['Image_Path'] = self.df.apply(lambda row: self.generate_filename(row), axis=1)
         self.df = self.df[self.df['Image_Path'].apply(lambda path: os.path.basename(path) in tiff_files)]
         self.df['Classification'] = None
+        self.df['Classified_Velocity'] = None
+        self.df['Second_Classification'] = None
 
     def generate_filename(self, row):
         return f"set01_{row['Participant']}_{row['Date']}_{row['Location']}_{row['Video']}_kymograph_{row['Capillary']}.tiff"
@@ -36,14 +40,22 @@ class KymographClassifier:
         return row['FPS'].values[0] if not row.empty else None
 
     def load_image(self):
+        if self.index is None or self.index >= len(self.df) or self.index < 0:
+            self.metadata_label.config(text="No more images to classify.")
+            self.panel.config(image='')
+            self.panel.image = None
+            return
+
         img_path = self.df.loc[self.index, 'Image_Path']
         image = cv2.imread(os.path.join(self.image_dir, img_path))
         fps = self.get_fps(self.df.loc[self.index, 'Participant'], self.df.loc[self.index, 'Date'], self.df.loc[self.index, 'Location'], self.df.loc[self.index, 'Video'])
         um_slope = self.df.loc[self.index, 'Velocity']
         if self.current_velocity_index == 0:
             average_slope = (um_slope * 2.44) / fps
+            self.original_velocity = um_slope
         else:
-            average_slope = (self.high_velocities[self.current_velocity_index] * 2.44) / fps
+            velocities = self.additional_velocities if self.use_additional_velocities else self.high_velocities
+            average_slope = (velocities[self.current_velocity_index - 1] * 2.44) / fps
         if self.inverted_slope:
             average_slope = -average_slope
         end_x = int((image.shape[0]-1) / average_slope) + int(image.shape[1] / 2)
@@ -59,57 +71,131 @@ class KymographClassifier:
         self.inverted_slope = not self.inverted_slope
         self.load_image()
 
+    def toggle_velocity_set(self, event):
+        self.use_additional_velocities = not self.use_additional_velocities
+        print("Using additional velocities" if self.use_additional_velocities else "Using high velocities")
+        self.load_image()
+
     def save_classification(self, classification):
-        self.df.at[self.index, 'Classification'] = classification
-        self.index += 1
+        if pd.isna(self.df.at[self.index, 'Classification']) or self.df.at[self.index, 'Classification'] == 'Correct':
+            self.df.at[self.index, 'Classification'] = classification
+        else:
+            self.df.at[self.index, 'Second_Classification'] = classification
+        if classification in ['Too Fast', 'Too Slow']:
+            velocities = self.additional_velocities if self.use_additional_velocities else self.high_velocities
+            self.df.at[self.index, 'Classified_Velocity'] = velocities[self.current_velocity_index - 1] if self.current_velocity_index != 0 else self.df.at[self.index, 'Velocity']
+        self.update_output_csv()
+
+    def finalize_classification(self):
+        self.df.at[self.index, 'Second_Classification'] = 'Correct'
+        self.update_output_csv()
+        self.index = self.find_next_unclassified()
         self.current_velocity_index = 0
-        if self.index < len(self.df):
+        if self.index is not None:
             self.load_image()
         else:
             print("No more images to classify.")
             self.root.destroy()
-            self.df.to_csv(self.csv_path, index=False)
-    
+
     def next_image(self):
-        if self.index + 1 < len(self.df):
-            self.index += 1
-            self.current_velocity_index = 0
+        self.index = self.find_next_unclassified(start_index=self.index + 1)
+        if self.index is not None:
             self.load_image()
         else:
-            print("No more images to classify.")
-            self.root.destroy()
-            self.df.to_csv(self.csv_path, index=False)
+            self.metadata_label.config(text="No more images to classify.")
+            self.panel.config(image='')
+            self.panel.image = None
 
     def previous_image(self):
-        if self.index > 0:
-            self.index -= 1
-            self.current_velocity_index = 0
+        self.index = self.index -1
+        if self.index is not None and self.index >= 0:
             self.load_image()
-    
-    def cycle_velocities(self):
-        self.current_velocity_index = (self.current_velocity_index + 1) % len(self.high_velocities)
-        self.load_image()
+        elif self.index < 0:
+            self.index = 0
+            self.load_image()
+        else:
+            self.metadata_label.config(text="No more images to classify.")
+            self.panel.config(image='')
+            self.panel.image = None
+
+    def set_velocity(self, event):
+        key = event.keysym
+        if key.isdigit():
+            key = int(key)
+            if key == 0:
+                self.current_velocity_index = 0
+            elif 1 <= key <= 9:
+                self.current_velocity_index = key
+            self.load_image()
 
     def setup_gui(self):
         self.root = tk.Tk()
         self.root.title("Kymograph Classification")
-        self.root.bind('<c>', lambda event: self.save_classification('Correct'))
+        self.root.bind('<c>', lambda event: self.finalize_classification())
         self.root.bind('<f>', lambda event: self.save_classification('Too Fast'))
-        self.root.bind('<s>', lambda event: self.cycle_velocities())  # Bind 's' to cycle through velocities
+        self.root.bind('<s>', lambda event: self.save_classification('Too Slow'))
         self.root.bind('<z>', lambda event: self.save_classification('Zero'))
         self.root.bind('<u>', lambda event: self.save_classification('Unsure'))
         self.root.bind('<p>', lambda event: self.toggle_slope())
         self.root.bind('<b>', lambda event: self.previous_image())
         self.root.bind('<n>', lambda event: self.next_image())
+        self.root.bind('<Shift_L>', self.toggle_velocity_set)
+        self.root.bind('<Shift_R>', self.toggle_velocity_set)
+        for i in range(10):
+            self.root.bind(str(i), self.set_velocity)
         self.panel = tk.Label(self.root)
         self.panel.pack(fill="both", expand="yes")
         self.metadata_label = tk.Label(self.root, text="", wraplength=400)
         self.metadata_label.pack()
-        if not self.df.empty:
+        self.index = self.find_next_unclassified()
+        if self.index is not None:
             self.load_image()
         else:
             print("No valid images found in the directory.")
         self.root.mainloop()
 
+    def initialize_output_csv(self):
+        if not os.path.exists(self.output_csv_path):
+            output_df = self.df[['Participant', 'Date', 'Location', 'Video', 'Image_Path', 'Velocity']]
+            output_df['Classified_Velocity'] = None
+            output_df['Classification'] = None
+            output_df['Second_Classification'] = None
+            output_df.to_csv(self.output_csv_path, index=False)
+        else:
+            self.df = pd.read_csv(self.output_csv_path)
+
+    def update_output_csv(self):
+        output_df = pd.read_csv(self.output_csv_path)
+        current_row = self.df.iloc[self.index]
+        output_df.loc[self.index] = [
+            current_row['Participant'],
+            current_row['Date'],
+            current_row['Location'],
+            current_row['Video'],
+            current_row['Image_Path'],
+            current_row['Velocity'],
+            current_row['Classified_Velocity'],
+            current_row['Classification'],
+            current_row['Second_Classification']
+        ]
+        output_df.to_csv(self.output_csv_path, index=False)
+
+    def find_next_unclassified(self, start_index=0):
+        for idx in range(start_index, len(self.df)):
+            if self.df.at[idx, 'Second_Classification'] != 'Correct':
+                return idx
+        return None
+
+    def find_previous_unclassified(self, start_index):
+        for idx in range(start_index, -1, -1):
+            if self.df.at[idx, 'Second_Classification'] != 'Correct':
+                return idx
+        return None
+
 if __name__ == "__main__":
-    classifier = KymographClassifier('C:\\Users\\gt8mar\\capillary-flow\\results\\kymographs\\tricky_kymographs', 'C:\\Users\\gt8mar\\capillary-flow\\metadata', 'C:\\Users\\gt8mar\\capillary-flow\\tricky_kymos.csv')
+    classifier = KymographClassifier(
+        'C:\\Users\\gt8mar\\capillary-flow\\results\\kymographs\\tricky_kymographs', 
+        'C:\\Users\\gt8mar\\capillary-flow\\metadata', 
+        'C:\\Users\\gt8mar\\capillary-flow\\tricky_kymos.csv',
+        'C:\\Users\\gt8mar\\capillary-flow\\classified_kymos.csv'
+    )
