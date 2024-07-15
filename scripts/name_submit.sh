f6690a68ff41af1ef9df7b92309038c1e54bbef9
gt8mar <gt8mar@yahoo.com>
2024-07-15 11:23:31 -0700

    bug fixes for renaming pipeline.
    Run name_submit and then make the renaming csv
    Then run rename_submit. Then kymos.

---
+++ scripts/name_submit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --job-name=capName
+#SBATCH --time=10:00:00
+#SBATCH --mem=150G
+#SBATCH --partition=cpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --output=cap_name_array_%A-%a.out
+#SBATCH --array=28-32
+
+cd /hpc/projects/capillary-flow/scripts
+module load anaconda
+conda activate capillary-flow
+echo "size: participant $SLURM_ARRAY_TASK_ID"
+python cap_name_pipeline.py ${SLURM_ARRAY_TASK_ID}
+echo "completed size part$SLURM_ARRAY_TASK_ID"
+exit
